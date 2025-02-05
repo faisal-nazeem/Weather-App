403d72bf326f4962ba1910ce73e094c9266701fc
faisal-nazeem <faisalhunzai554@.com>
2025-02-05 20:17:54 +0500

    feat: Add reusable components and Intro screen

---
+++ app/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import Label from './src/Components/label';
+import color from './src/Components/color';
+import Button from './src/Components/Button';
+import { useRouter } from 'expo-router';
+
+
+const Index = () => { 
+
+  const router =useRouter()
+  return (
+    <View style={styles.container}>
+      <Image source={require('../app/Image/Weather.png')} />
+
+      <View style={styles.contect}>
+        <Label text='Weather' fontSize={80} fontWeight='700' color={color.white} />
+        <Label text='ForeCasts' fontSize={60} fontWeight='400' color={color.Goldenrod} />
+      </View>
+
+      <View>
+        <Button
+          title='Get Start'
+          onPress={() => router.push('/src/screen/temperatureScreen')}
+          style={styles.startButton}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'darkblue',
+  },
+  contect: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  startButton: {
+    width: 304,
+    height: 72,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 30
+
+  }
+});
---
+++ app/src/Components/label.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextStyle } from 'react-native';
+
+interface LabelProps {
+  text?: string;
+  fontSize?: number; 
+  fontWeight?: 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';  
+  color?: string; 
+  style?: TextStyle;  
+}
+
+const Label: React.FC<LabelProps> = ({ text, fontSize, fontWeight, color, style }) => (
+  <View style={styles.container}>
+    <Text style={[styles.text, { fontSize, fontWeight, color }, style]}>{text}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent :'center',
+    alignItems :'center'
+  },
+  text: {
+    fontSize: 14,  
+    fontWeight: 'normal', 
+   
+  },
+});
+
+export default Label;
